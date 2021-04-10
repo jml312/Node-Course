df25122a0e01797654374a7ca6b68b84c09a9fcf
Josh <jml312@case.edu>
2021-04-10 00:06:36 -0400

    updated

---
+++ chat-app/src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  console.log("New Websocket connection");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
